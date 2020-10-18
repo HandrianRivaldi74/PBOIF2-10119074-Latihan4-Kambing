fbc86c56aba17e38ec15a88943895bb73f921ad0
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 13:36:40 +0700

    Latihan4

---
+++ src/pboif2_10119074_latihan4/Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan4;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Sistem pertambahan
+ */
+public class Kambing {
+    
+    public void tambahKambing() {
+        //Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " +
+                        jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+}
